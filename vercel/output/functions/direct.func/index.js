ffc16b9f21d9c49475249b8c6c5451a9fd20f050
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-05 12:34:45 -0500

    Fixed function url access

--- vercel/output/functions/direct.func/index.js
+++ vercel/output/functions/direct.func/index.js
@@ -1,16 +1,16 @@
 import { renderPage } from "vite-plugin-ssr";
 
 export default async function handler(request, response) {
-  console.log("Request to url:", request.url);
+  console.log("Request to url:", request.originalUrl);
 
-  const pageContext = await renderPage({ url: request.url });
+  const pageContext = await renderPage({ url: request.originalUrl });
   const { httpResponse } = pageContext;
 
   if (!httpResponse) {
     response.statusCode = 200;
     response.end();
   } else {
-    const { body, statusCode, contentType } = httpResponse;;
+    const { body, statusCode, contentType } = httpResponse;
 
     response.statusCode = statusCode;
     response.setHeader("Content-Type", contentType);
--- vercel/output/functions/refresh.func/index.js
+++ vercel/output/functions/refresh.func/index.js
@@ -1,19 +1,3 @@
-import { renderPage } from "vite-plugin-ssr";
-
 export default async function handler(request, response) {
-  console.log("Request to url:", request.url);
-
-  const pageContext = await renderPage({ url: request.url });
-  const { httpResponse } = pageContext;
-
-  if (!httpResponse) {
-    response.statusCode = 200;
-    response.end();
-  } else {
-    const { body, statusCode, contentType } = httpResponse;;
-
-    response.statusCode = statusCode;
-    response.setHeader("Content-Type", contentType);
-    response.end(body);
-  }
+  console.log("Request to refresh");
 }
