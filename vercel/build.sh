9b10621a5eb1ddff0bf604a881b4ab3b044be90b
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-05 13:20:19 -0500

    Fixed forgot to change path in build.sh

--- vercel/build.sh
+++ vercel/build.sh
@@ -35,7 +35,7 @@ cp -a dist/client/. .vercel/output/static
 # (This ensures all dependencies are bundled into the file.)
 cd .vercel/output/functions
 npx ncc build --minify --out ./direct.func direct.func/index.js
-npx ncc build --minify --out ./prerender.func prerender.func/index.js
+npx ncc build --minify --out ./index.func index.func/index.js
 
 # ————————————————————————————————————————————————————————————————————————————————
 
