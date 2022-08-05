b17eae3f46f7b26310162b6c0bfd4f8034a4e6be
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-05 16:07:54 -0500

    Updated edge functions

--- vercel/output/functions/direct.func/index.js
+++ vercel/output/functions/direct.func/index.js
@@ -1,8 +1,8 @@
 import { renderPage } from "vite-plugin-ssr";
 
 /**
- * @param {import("@vercel/node").VercelRequest} request 
- * @param {import("@vercel/node").VercelResponse} response 
+ * @param {import("@vercel/node").VercelRequest} request
+ * @param {import("@vercel/node").VercelResponse} response
  */
 export default async function handler(request, response) {
   console.log("[direct]");
--- vercel/output/functions/test.func/index.js
+++ vercel/output/functions/test.func/index.js
@@ -1,7 +1,11 @@
-export default function index(request, event) {
+/**
+ * @param {Request} request
+ */
+export default async function index(request, event) {
   console.log("[test]");
-  console.log(JSON.stringify(request, null, 2));
-  console.log(JSON.stringify(event, null, 2));
+  console.log(request.url);
+  console.log(Array.from(request.headers.entries()));
+  console.log(await request.text());
 
   return new Response(`Hello, from the Edge test!`);
 }
