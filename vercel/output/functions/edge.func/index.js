f1ed5437d9b5c7723a127d49c3e794a83c316f10
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-05 15:51:54 -0500

    Try out edge functions

--- vercel/output/functions/edge.func/index.js
+++ vercel/output/functions/edge.func/index.js
@@ -1,7 +1,7 @@
 export default function index(request, event) {
-  console.log("[test]");
+  console.log("[edge]");
   console.log(JSON.stringify(request, null, 2));
   console.log(JSON.stringify(event, null, 2));
 
-  return new Response(`Hello, from the Edge test!`);
+  return new Response(`Hello, from the Edge!`);
 }
