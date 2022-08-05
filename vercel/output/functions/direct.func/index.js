cd2586f6544d4362431fc938df774e8a2dbcc645
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-05 15:25:17 -0500

    Added debug stuff to direct function too

--- vercel/output/functions/direct.func/index.js
+++ vercel/output/functions/direct.func/index.js
@@ -5,7 +5,12 @@ import { renderPage } from "vite-plugin-ssr";
  * @param {import("@vercel/node").VercelResponse} response 
  */
 export default async function handler(request, response) {
-  console.log("[direct]", request.url);
+  console.log("[default]");
+  console.log(request.method, request.url, request.httpVersion);
+  console.log(JSON.stringify(request.query, null, 2));
+  console.log(JSON.stringify(request.headers, null, 2));
+  console.log(JSON.stringify(request.cookies, null, 2));
+  console.log(request.body);
 
   const pageContext = await renderPage({ url: request.url });
   const { httpResponse } = pageContext;
