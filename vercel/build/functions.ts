8f077fc0556317ba51e0456d60e0920cf2de8662
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-17 15:35:20 -0500

    Improve test functions transformer 3

--- vercel/build/functions.ts
+++ vercel/build/functions.ts
@@ -17,7 +17,13 @@ const configText = dedent`
 
 const scriptText = dedent`
   export default async function handler(request, response) {
-    response.send(\`[test] \${JSON.stringify(request, null, 2)}\`);
+    console.log("[test]");
+    console.log(request.method, request.url, request.httpVersion);
+    console.log(JSON.stringify(request.query, null, 2));
+    console.log(JSON.stringify(request.headers, null, 2));
+    console.log(JSON.stringify(request.cookies, null, 2));
+    console.log(request.body);
+    response.send("[test] " + request.url);
   }
 `;
 
