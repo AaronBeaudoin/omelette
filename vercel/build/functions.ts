12a0ec88e4c2f453fe435f4b04631f32b71b82fc
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-17 17:00:40 -0500

    Remove comments in script template

--- vercel/build/functions.ts
+++ vercel/build/functions.ts
@@ -21,14 +21,6 @@ const template = dedent`
   import { $handler as inner } from "$path";
 
   export default async function handler(request: VercelRequest, response: VercelResponse) {
-    // console.log("[test]");
-    // console.log(request.method, request.url, request.httpVersion);
-    // console.log(JSON.stringify(request.query, null, 2));
-    // console.log(JSON.stringify(request.headers, null, 2));
-    // console.log(JSON.stringify(request.cookies, null, 2));
-    // console.log(request.body);
-    // response.send("[test] " + request.url);
-
     try {
       let handlerResult: unknown = inner(request.query);
       response.statusCode = 200;
