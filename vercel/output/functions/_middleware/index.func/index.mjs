be14c372c6f6a873eee417cc1b0b82f2bb10212c
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-18 16:38:37 -0500

    Test middleware rewrite 7

--- vercel/output/functions/_middleware/index.func/index.mjs
+++ vercel/output/functions/_middleware/index.func/index.mjs
@@ -3,10 +3,12 @@
  * @param {Event} event
  */
 export default async function index(request) {
-  console.log("hello from middleware");
+  const url = new URL(request.url);
+  url.pathname = "/";
+  console.log(url.toString());
 
   const response = new Response();
   // response.headers.set("x-middleware-next", "1");
-  response.headers.set("x-middleware-rewrite", "/");
+  response.headers.set("x-middleware-rewrite", url.toString());
   return response;
 }
