930efaf68933c0ec7b9a05092960d057f548ac7e
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-29 16:05:48 -0500

    Fix middleware

--- vercel/output/functions/_middleware/index.func/index.mjs
+++ vercel/output/functions/_middleware/index.func/index.mjs
@@ -3,13 +3,10 @@
  * @param {Event} event
  */
 export default function middleware(request) {
-  const url = new URL(request.url);
-  url.pathname = "/";
-  console.log(url.toString());
+  const path = new URL(request.url).pathname;
+  const rewrite = `/_functions/${path.split("/").slice(2).join("/")}`;
 
   const response = new Response();
-  // response.headers.set("x-middleware-next", "1");
-  response.headers.set("x-middleware-rewrite", "/");
-  response.headers.set("x-spaghetti", "yes");
+  response.headers.set("x-middleware-rewrite", rewrite);
   return response;
 }
