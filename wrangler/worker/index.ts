9471a759cc60511136164718c7000e10dd9b5cab
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-09-14 16:12:46 -0500

    Minor formatting

--- wrangler/worker/index.ts
+++ wrangler/worker/index.ts
@@ -14,12 +14,12 @@ async function handleRequest(request: Request, env: Environment, context: Execut
   // 2. Respond with a static asset if applicable.
   response = await handleAssetRoute(resource, env, context);
   if (response) return response;
-  
+
   // 3. Respond with a page if applicable.
   response = await handlePageRoute(resource, env, context);
   if (response) return response;
 
-  // 4. Fallback to a minimal 404 page.
+  // 4. Fallback to a minimal 500 page.
   // This code should never be executed under normal circumstances
   // because the page route handler should always return a response.
   return new Response("END_OF_WORKER", { status: 500 });
