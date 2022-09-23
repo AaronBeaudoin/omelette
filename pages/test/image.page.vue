fd0180895ed9564872080f5d4d9ef133cad38be7
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-09-23 10:55:08 -0500

    Added relevant links

--- pages/test/image.page.vue
+++ pages/test/image.page.vue
@@ -1,3 +1,5 @@
 <template>
+  <!-- https://github.com/brillout/vite-plugin-ssr/issues/462 -->
+  <!-- https://github.com/vitejs/vite/issues/10223 -->
   <img :src="`/test/_cat`" alt="cat">
 </template>
